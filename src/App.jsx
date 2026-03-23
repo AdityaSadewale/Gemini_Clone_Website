248b493d62e8cc761bb2dec87e3082c48a7d619e
ADITYA_SADEWALE <adityasadewale@gmail.com>
2026-03-23 22:34:53 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Main from "./components/Main/Main"; // Fixed import path
 
+
 const App = () => {
   return (
     // overflow: hidden prevents the whole page from scrolling
